fc40ee95a4b145c6d5dcee4eb0a172c2a5dc1cad
kawr13 <62603910+kawr13@users.noreply.github.com>
2023-10-22 06:34:33 +0500

    Merge branch 'blogmoby' of https://github.com/kawr13/SmartLearn-Squad into blogmoby
    # Conflicts:
    #	SmartLearn/ProfileApp/templates/registration/password_reset_email.html
    #	SmartLearn/ProfileApp/urls.py
    #	SmartLearn/Project/urls.py
    #	SmartLearn/db.sqlite3

--- SmartLearn/CabinetApp/urls.py
+++ SmartLearn/CabinetApp/urls.py
@@ -7,7 +7,6 @@ app_name = 'cabinet'
 #
 urlpatterns = [
     path('get_events/<int:cabinet_id>/', get_events, name='get_events'),
-    # path('calendar/', calendar_view, name='calendar'),
     path('cabinets/<int:cab_id>/', cabinets_view, name='cabinet'),
     path('get_updates/<int:cabinet_id>/', update_calend, name='get_update'),
     path('userfull_info_cab/<int:cabinet_id>/', user_full, name='userfull_info_cab'),
