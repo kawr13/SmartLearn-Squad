3accfe27d7e61bcd81a94704df5066e942b703a6
Tooliy <tuvanchap_tumen@mail.ru>
2023-11-08 04:00:36 +0700

    new branch

--- SmartLearn/Project/settings.py
+++ SmartLearn/Project/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'Project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'SmartLearn',
+        'USER': 'Tumen',
+        'PASSWORD': 'SulaSorok5!',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
